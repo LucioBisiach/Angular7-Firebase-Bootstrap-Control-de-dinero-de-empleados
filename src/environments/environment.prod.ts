668eea17d16d2c0bf66b5f7828351e260b912495
LucioBisiach <34654675+LucioBisiach@users.noreply.github.com>
2019-11-16 16:43:50 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,6 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyDQ6mzXro9J-06YZjo0b3AUk3-PB5s4eMo",
-    authDomain: "cuentas-prod-3bbc2.firebaseapp.com",
-    databaseURL: "https://cuentas-prod-3bbc2.firebaseio.com",
-    projectId: "cuentas-prod-3bbc2",
-    storageBucket: "cuentas-prod-3bbc2.appspot.com",
-    messagingSenderId: "551070805175",
-    appId: "1:551070805175:web:0fe496ec2071239cd27132"
+
   }
 };
